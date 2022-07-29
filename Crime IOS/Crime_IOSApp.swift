afe41973a9bbb5ea58f415a3e6b9e917a10c58ee
Linda Xue <lexue28@gmail.com>
2022-07-29 14:41:30 -0400

    Initial Commit

---
+++ Crime IOS/Crime_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crime_IOSApp.swift
+//  Crime IOS
+//
+//  Created by Linda Xue on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Crime_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
